26927c091dc835c848aa8310308d60dbc64dc379
sarthexo <148628132+sarthexo@users.noreply.github.com>
2023-12-03 11:04:52 +0000

    variables

---
+++ basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID=23456
+let accEmail="sl@gmail.com"
+var accType="debit"
+
+accPsswrd="jofewe"  //we can even declare the value without datatype but we should not it is not a good practice
+
+//accountID=23 //cannot reassign to const bcoz is is fixed
+accEmail="kjot@gmail.com"  // we can change the values of var and let  afterwards
+
+accType="credit"  // we can change the values of var and let  afterwards 
+console.log(accountID);  //we have to write console.log in semicolon
+
+console.log(accEmail);
+console.log(accType); // now we get changed values of var and let
+

a41283233dc68eb912356c742999a08b2232022b
sarthexo <148628132+sarthexo@users.noreply.github.com>
2023-12-03 12:55:02 +0000

    stringuse

---
+++ basics/03_stringuse.js
@@ -0,0 +1,15 @@
+let str1="hello"
+let str2="sarthak"
+
+let str3=str1+str2
+
+console.log(str3);  //we can add 2 string like this
+
+
+console.log("1"+ 2); // here because we wrote the strig first so it got concatenated with the number and printed as 12
+console.log(1+2+"3");// here bcoz we didnt write first 2 numbers a string the got added and the 3rd got concatenated with them
+
+let gamecounter=100
+gamecounter ++ //here the number got post incremanted
+
+console.log(gamecounter);

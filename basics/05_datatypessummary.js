6a99a808d752afcf7328f2a27c63c2e1c46856be
sarthexo <148628132+sarthexo@users.noreply.github.com>
2023-12-03 13:43:21 +0000

    datatypesummaryinjs

---
+++ basics/05_datatypessummary.js
@@ -0,0 +1,33 @@
+
+
+const score=100
+
+const scoreValue=100.3
+
+const isLoggedin= false
+const outsideTemp=null
+let userEmail
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+
+//reference types
+//Array,Functions,Objects
+
+const heros=["shaktiman","sarthak","superman"]   //array
+console.log(heros);
+
+let myObj={   //using of object
+    name:"sarthak",
+    age:22   //we canwrite datatype of anyvalue like string ,int,float etc within the object
+}
+console.log(myObj);
+
+
+const myFunction=function(){
+    console.log("hellow world");
+}
+
+myFunction()  //prinitng the value 
+//console.log(myFunction);

953d560bd09206faccb937ff2da2a28bf4297796
sarthexo <148628132+sarthexo@users.noreply.github.com>
2023-12-03 12:05:08 +0000

    datatypes

---
+++ basics/02_Datatypes.js
@@ -0,0 +1,29 @@
+"use strict";//treats all js code as newer version
+//alert(3+3)  cannot do this we are using nodejs, not browser
+
+console.log(3+
+                    // we should not write code like this we should improve the readiblity of the code and make readibility higher
+    3)
+
+    console.log("Sarthak");
+
+    let name="sarthak"
+    let age=23
+    let isLogggedin=false
+
+    //number=> 2 to the power 53
+    //bigInt
+    //string => ""
+    //boolean => true/false
+    //null => standalone value
+    //unidentified =>
+    //symbol => unique
+
+    //object 
+
+    console.log(typeof "sarthak");
+    
+    console.log(typeof null);  //object
+
+    
+    console.log(typeof undefined);  //undefined
